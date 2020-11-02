33bcf5f47f80541bc5ccc5e904b6d24becececca
Kirill Privalenkov <privalenkovkirill@gmail.com>
2020-11-02 21:58:24 +0300

    first commit

---
+++ js/cursor.js
@@ -0,0 +1,6 @@
+const cursor = document.querySelector('#cursor')
+const hand_img = document.querySelector('#hand-img')
+
+document.addEventListener('mouseup', () => {
+    hand_img.src = '../img/'
+})
---
+++ js/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.container__btn')
+
+const socket = io('//178.154.234.248:3000');
+let clicks = 0
+
+function UpdateClick(cc) {
+    clicks = cc
+    document.querySelector('.container__counter__number').textContent = clicks
+}
+
+function AddClick() {
+    UpdateClick(clicks + 1)
+    socket.emit('click')
+}
+
+socket.on('connection', UpdateClick)
+socket.on('click', UpdateClick)
+
+btn.addEventListener('click', () => {
+    AddClick()
+})
