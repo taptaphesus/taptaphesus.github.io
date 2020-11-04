d4d0fac259783304d72e640583f01f95ee083523
Kirill Privalenkov <privalenkovkirill@gmail.com>
2020-11-04 22:35:27 +0300

    addparticles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const btn = document.querySelector('.container__btn')
 
 const socket = io()
 let clicks = 0
+let lastClick = 0
 
 function UpdateClick(cc) {
     clicks = cc
@@ -15,15 +16,39 @@ function AddClick() {
 
 socket.on('connection', UpdateClick)
 socket.on('click', UpdateClick)
+// let interval, t
+// function intervalClick() {
+//     audio.play()
+//     var d = new Date()
+//     t = d.getTime()
+//     interval = t - lastClick
+//     console.log(t + " | " + interval + " | " + audio.playbackRate)
+    
+//     if(interval <= 150) {
+//         audio.playbackRate = 2.0;
+//     } 
+//     else if (interval >= 250 && interval <= 500) {
+//         audio.playbackRate = 1.0;
+//     }
+//     else if (interval >= 500 && interval <= 700) {
+//         audio.playbackRate = 0.5;
+//     }
+//     else if(interval >= 700) {
+//             audio.pause()
+//     }
+
+//     lastClick = t
+// }
 
 btn.addEventListener('click', () => {
+    audio.play()
+    // intervalClick()
     AddClick()
 })
 btn.addEventListener('mouseup', (e) => {
     btn.style.backgroundImage = `url('img/hesus.png')`
 })
 btn.addEventListener('mousedown', (e) => {
-    audio.play()
     btn.style.backgroundImage = `url('img/hesus_tap.png')`
 })
 btn.addEventListener('mouseleave', () => {
